b51e20a74e7b17b49ef4341bbfe586c5092bb4ea
sahil1sk <khannasahil303@gmail.com>
2020-12-20 12:48:53 +0530

    Button, icons, visibilit & breakpoints

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,41 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolore iusto modi. Tenetur praesentium debitis laborum quibusdam ipsa omnis repellat maiores a quia similique veritatis quisquam, eos libero, architecto quas.</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab asperiores velit culpa quasi nemo dolores laborum accusamus ullam excepturi assumenda aperiam atque voluptas ut molestiae, repellendus, delectus animi dicta quia.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subheading font-weight-bold">This is subheading</p>
-    <p class="caption">this is a caption</p>
+
+    <!-- so this button will hidden on medium to small sized screens -->
+    <v-btn class="hidden-md-and-down">click me</v-btn>
+
+    <!-- shown on medium and small scrren -->
+    <v-btn class="hidden-md-and-up">click me</v-btn>
+
+    <!-- hidden on small screens only -->
+    <v-btn class="hidden-sm-only">click me</v-btn>
+
+    <v-btn class="pink lighten-2 white--text">click me</v-btn>
+    <v-btn depressed color="red">click me</v-btn>
+    <v-btn color="pink">click me</v-btn>
+
+
+    <v-btn class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn large class="pink white--text">
+      <v-icon left large>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+    
+    <v-btn fab depressed small dark color="purple">
+      <v-icon small>favorite</v-icon>
+    </v-btn>
+    
+
   </div>
 </template>
 
