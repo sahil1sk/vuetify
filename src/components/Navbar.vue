3a4f9a6a5e4cd2be0bdaa061cc7beaf7866f9c1c
sahil1sk <khannasahil303@gmail.com>
2020-12-20 16:46:34 +0530

    Adding routing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,14 +8,27 @@
             <span>Ninja</span>
         </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn depressed class="transparent text-grey font-weight-light">
+        <v-btn depressed class="transparent text--grey font-weight-light">
             <span>Sign Out</span>
             <v-icon right>exit_to_app</v-icon>
         </v-btn>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" app class="indigo">
-        <p>test</p>
+    <v-navigation-drawer v-model="drawer" app class="primary">
+        <v-list>
+            <v-list-item v-for="(link, i) in links" :key="i" router :to="link.route">
+                <v-list-item-action>
+                    <v-icon class="white--text">
+                        {{ link.icon }}
+                    </v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title class="white--text">
+                        {{ link.text }}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
     </v-navigation-drawer>
 
   </nav>
@@ -25,7 +38,12 @@
 export default {
     data() {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'folder', text: 'My Projects', route: '/projects' },
+                { icon: 'person', text: 'Team', route: '/team' },
+            ]
         }
     }
 }
