e2d69eeffe4b348d07bd262b3fffac5af2b051c2
sahil1sk <khannasahil303@gmail.com>
2020-12-21 07:42:34 +0530

    tooltip

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,43 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+      <v-row class="mb-3">
+          
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              small
+              depressed
+              v-bind="attrs"
+              v-on="on"
+              color="grey--text"
+              @click="sortBy('title')"
+            >
+              <v-icon left small>folder</v-icon>
+              <span class="caption text-lowercase">By project name</span>
+            </v-btn>
+          </template>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+
+        
+        <v-tooltip top>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              small
+              depressed
+              v-bind="attrs"
+              v-on="on"
+              color="grey--text"
+              @click="sortBy('person')"
+            >
+              <v-icon left small>person</v-icon>
+              <span class="caption text-lowercase">By person</span>
+            </v-btn>
+          </template>
+          <span>Sort projects by person name</span>
+        </v-tooltip>
+      </v-row>
       
       <v-card :class="`project ${project.status}`" flat v-for="(project, i) in projects" :key="i">
         <v-row class="pa-3">
@@ -42,6 +79,11 @@ export default {
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+    }
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
     }
   }
 }
